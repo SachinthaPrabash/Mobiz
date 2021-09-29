04793c8f217466db331c7ced9ddb61e138b7cc36
sachintha prabash <57107463+SachinthaPrabash@users.noreply.github.com>
2021-09-30 01:52:19 +0530

    Frontend home Screen and product page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
-function App() {
+
+const App = () => {
   return (
-    <div >
-      Welcome to MOBIZ
-    </div>
+    < >
+      <Header />
+      <main className='py-4'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
