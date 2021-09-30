18f6ac01203100b54b36a6f6a11060b5438c3b64
sachintha prabash <57107463+SachinthaPrabash@users.noreply.github.com>
2021-10-01 02:16:13 +0530

    frontend and backend connect with express, nodemon , create env

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -6,35 +6,35 @@ const Rating = ({ value, text, color }) => {
     return (
         <div className='rating'>
             <span>
-                <i style={{ color: color }}
+                <i style={{ color }}
                     className={value >= 1 ? 'fas fa-star'
                         : value >= 0.5 ? 'fas fa-star-half-alt'
                             : 'far fa-star'}>
                 </i>
             </span>
             <span>
-                <i style={{ color: color }}
+                <i style={{ color }}
                     className={value >= 2 ? 'fas fa-star'
                         : value >= 1.5 ? 'fas fa-star-half-alt'
                             : 'far fa-star'}>
                 </i>
             </span>
             <span>
-                <i style={{ color: color }}
+                <i style={{ color }}
                     className={value >= 3 ? 'fas fa-star'
                         : value >= 2.5 ? 'fas fa-star-half-alt'
                             : 'far fa-star'}>
                 </i>
             </span>
             <span>
-                <i style={{ color: color }}
+                <i style={{ color }}
                     className={value >= 4 ? 'fas fa-star'
                         : value >= 3.5 ? 'fas fa-star-half-alt'
                             : 'far fa-star'}>
                 </i>
             </span>
             <span>
-                <i style={{ color: color }}
+                <i style={{ color }}
                     className={value >= 5 ? 'fas fa-star'
                         : value >= 4.5 ? 'fas fa-star-half-alt'
                             : 'far fa-star'}>
@@ -49,10 +49,10 @@ Rating.defaultProps = {
     color: '#f8e825',
 }
 
-Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string,
-}
+// Rating.propTypes = {
+//     value: PropTypes.number.isRequired,
+//     text: PropTypes.string.isRequired,
+//     color: PropTypes.string,
+// }
 
 export default Rating
