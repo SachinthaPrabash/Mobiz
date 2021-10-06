3c1e77ff913c624382a4744c54cc4bfc4d816c87
sachintha prabash <57107463+SachinthaPrabash@users.noreply.github.com>
2021-10-07 00:43:30 +0530

    shipping page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 const App = () => {
   return (
@@ -23,6 +24,8 @@ const App = () => {
             <Route path='/' component={HomeScreen} exact />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
+            <Route path='/shipping' component={ShippingScreen} />
+
           </Container>
         </main>
         <Footer />
