62e37884d11f46193ad90fffff8622a7ec4d7ef4
sachintha prabash <57107463+SachinthaPrabash@users.noreply.github.com>
2021-10-13 21:26:36 +0530

    Edit product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 const App = () => {
   return (
@@ -36,10 +37,8 @@ const App = () => {
             <Route path='/order/:id' component={OrderScreen} />
             <Route path='/admin/userlist' component={UserListScreen} />
             <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+            <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
             <Route path='/admin/productlist' component={ProductListScreen} />
-
-
-
 
           </Container>
         </main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducers, productDetailsReducers, productDeleteReducers } from './reducers/productReducers'
+import {
+    productListReducers,
+    productDetailsReducers,
+    productDeleteReducers,
+    productCreateReducers,
+} from './reducers/productReducers'
 import { cartReducers } from './reducers/cartReducers'
 import {
     userLoginReducers,
@@ -21,6 +26,7 @@ const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducers,
     productDelete: productDeleteReducers,
+    productCreate: productCreateReducers,
     cart: cartReducers,
     userLogin: userLoginReducers,
     userRegister: userRegisterReducers,
